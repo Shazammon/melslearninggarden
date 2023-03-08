55c99be97e784d13c0650ae27ccefc7ff4a194d3
Shazammon <ammonorgill@gmail.com>
2023-03-08 14:12:26 -0700

    updates to NavBar

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -1,12 +1,39 @@
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+    const navBarOptions = [
+        {
+            'navBarName': 'Classes', 
+            'linkName': 'class'
+        },
+        {
+            'navBarName': 'Schedule',
+            'linkName':'schedule'
+        }
+        //  'Tuition Cost', 'About Me', 'Make a Payment'
+    ]
+
     return (
         <nav>
             <ul>
                 <li>
-                    <Link>Ammon</Link>
+                    <Link
+                        to="/"
+                    >
+                        Mel's Learning Garden
+                    </Link>
                 </li>
+                {navBarOptions.map((option) => {
+                    return (
+                        <li>
+                            <Link
+                                to={option.linkName}
+                            >
+                                {option.navBarName}
+                            </Link>
+                        </li>
+                    )
+                })}
             </ul>
         </nav>
     )
