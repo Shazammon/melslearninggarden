2512c78036958d6de950736434d07520afd74344
Shazammon <ammonorgill@gmail.com>
2023-03-08 16:35:29 -0700

    downloaded tailwind package

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
+    const classOptions = ['Cooking', 'Gardening']
+
     return (
-        <>
-            <div>This is the Home page</div>
-        </>
-    )
+
+      <>
+        <h1>Mel's Learning Garden</h1>
+        <h3>Hands-On Learning & Activities for Kids & Teens</h3>
+        <p>
+          From creating beautiful music and reading the best literature, to
+          cooking up culinary creations from homegrown garden-fresh fruits and
+          vegetables, to building positive physical and mental health and
+          confidence through connection and the practice of true principles,
+          this is the place where hearts and minds are nurtured.
+        </p>
+        <h2>Classess Offered</h2>
+        <ul>
+            {classOptions.map((option) => {
+                return (
+                    <li>
+                        <Link
+                        >
+                            {option}
+                        
+                        </Link>
+                    </li>
+                )
+            })}
+        </ul>
+      </>
+    );
 }
