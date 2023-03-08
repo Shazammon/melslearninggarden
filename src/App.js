3dc68840b1c3c11a61e894300a60fd44585e4007
Shazammon <ammonorgill@gmail.com>
2023-03-08 12:38:12 -0700

    set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Navbar from './components/partials/Navbar'
+
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route 
+            path="/"
+            element={<Home />}
+        />
+      </Routes>
+      <Home />
+
+    </Router>
   );
 }
 
