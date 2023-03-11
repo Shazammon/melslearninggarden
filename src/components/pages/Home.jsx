b979abbbd4e83877c85ace91d2947c913b55d8a7
Shazammon <ammonorgill@gmail.com>
2023-03-10 17:16:54 -0700

    styling updates

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,27 +7,29 @@ export default function Home() {
       <>
         <p>Welcome to Mel's Learning Garden</p>
         <div className="flex-testing">
-          <div className="flex-child">
+          {/* <div className="flex-child">
             <div>Hiiiiii</div>
-          </div>
-          <div className="flex-child">
+            </div>
+            <div className="flex-child">
             <div>Helloooo</div>
-          </div>
+        </div> */}
+            <img
+            src="./gardening_preschool.jpg"
+            alt="preschooler gardening"
+            className="yes"
+            />
+            <div>
+                <h3>Hands-On Learning & Activities for Kids & Teens</h3>
+                <p>
+                From creating beautiful music and reading the best literature, to
+                cooking up culinary creations from homegrown garden-fresh fruits and
+                vegetables, to building positive physical and mental health and
+                confidence through connection and the practice of true principles,
+                this is the place where hearts and minds are nurtured.
+                </p>
+
+            </div>
         </div>
-        <h3>Hands-On Learning & Activities for Kids & Teens</h3>
-        <img
-          src="./gardening_preschool.jpg"
-          alt="preschooler gardening"
-          className="yes"
-          // height='auto'
-        />
-        <p>
-          From creating beautiful music and reading the best literature, to
-          cooking up culinary creations from homegrown garden-fresh fruits and
-          vegetables, to building positive physical and mental health and
-          confidence through connection and the practice of true principles,
-          this is the place where hearts and minds are nurtured.
-        </p>
         <h2>Classess Offered</h2>
         <ul>
           {classOptions.map((option) => {
