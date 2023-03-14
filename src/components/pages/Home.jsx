794f2668f0e304e3cf7fe3d34f90549642d6193e
Shazammon <ammonorgill@gmail.com>
2023-03-14 13:31:31 -0600

    updated title page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -13,22 +13,26 @@ export default function Home() {
             <div className="flex-child">
             <div>Helloooo</div>
         </div> */}
-            <img
+          <img
             src="./gardening_preschool.jpg"
             alt="preschooler gardening"
             className="img-one"
-            />
-            <div className="div-two">
-                <h3>Hands-On Learning & Activities for Kids & Teens</h3>
-                <p>
-                From creating beautiful music and reading the best literature, to
-                cooking up culinary creations from homegrown garden-fresh fruits and
-                vegetables, to building positive physical and mental health and
-                confidence through connection and the practice of true principles,
-                this is the place where hearts and minds are nurtured.
-                </p>
-
-            </div>
+          />
+          <div className="div-two">
+            <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+              <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                Hands-On Learning & Activities
+              </span>{" "}
+              for Kids & Teens.
+            </h1>
+            <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+              From creating beautiful music and reading the best literature, to
+              cooking up culinary creations from homegrown garden-fresh fruits
+              and vegetables, to building positive physical and mental health
+              and confidence through connection and the practice of true
+              principles, this is the place where hearts and minds are nurtured.
+            </p>
+          </div>
         </div>
         <h2>Classess Offered</h2>
         <ul>
