64b24156f59e4399c22d0174777293322df8850f
Shazammon <ammonorgill@gmail.com>
2023-03-22 08:50:20 -0600

    more updates to ClassSmall and Home components

--- src/components/comps/ClassSmall.jsx
+++ src/components/comps/ClassSmall.jsx
@@ -24,7 +24,8 @@ export default function ClassSmall() {
             <div>{classData[0].schedule_teens}</div>
             <div>{classData[0].cost}</div>
             <div>{classData[0].class_photos}</div>
-            <div>{classData[0].class_photos}</div>
+            <div>{classData[0].schedule_teens}</div>
+            <div>{classData[0].cost}</div>
             <div>{classData[0].class_photos}</div>
         </>
     )
