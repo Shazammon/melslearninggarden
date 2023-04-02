2231079db5a1b6d01a5ff88fb223bee00871ec3c
Shazammon <ammonorgill@gmail.com>
2023-04-02 15:38:21 -0400

    updated mapping function

--- src/components/comps/ClassSmall.jsx
+++ src/components/comps/ClassSmall.jsx
@@ -26,6 +26,9 @@ export default function ClassSmall() {
             <div>{classData[0].schedule_teens}</div>
             <div>{classData[0].cost}</div>
             <div>{classData[0].class_photos}</div>
+            <div>{classData[0].schedule_teens}</div>
+            <div>{classData[0].cost}</div>
+            <div>{classData[0].class_photos}</div>
 
         </>
     )
