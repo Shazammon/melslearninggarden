8b68ce60043f47e328bc3e8c5f0cea5513804ca0
Shazammon <ammonorgill@gmail.com>
2023-04-08 14:09:31 -0400

    updated AboutMe component

--- src/components/comps/ClassSmall.jsx
+++ src/components/comps/ClassSmall.jsx
@@ -29,6 +29,13 @@ export default function ClassSmall() {
             <div>{classData[0].schedule_teens}</div>
             <div>{classData[0].cost}</div>
             <div>{classData[0].class_photos}</div>
+            <div>{classData[0].schedule_elementary}</div>
+            <div>{classData[0].schedule_teens}</div>
+            <div>{classData[0].cost}</div>
+            <div>{classData[0].class_photos}</div>
+            <div>{classData[0].schedule_teens}</div>
+            <div>{classData[0].cost}</div>
+            <div>{classData[0].class_photos}</div>
 
         </>
     )
